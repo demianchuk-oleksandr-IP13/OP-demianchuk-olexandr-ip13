0dd9430a211972163c3195ac0765c75d54ecebe5
demianchuk-oleksandr-IP13 <90649585+demianchuk-oleksandr-IP13@users.noreply.github.com>
2021-09-29 00:15:10 +0300

    Update Program.cs

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -5,24 +5,30 @@ namespace lab1
     {
         static void Main(string[] args)
         {
-            double a1, b1, c1, a2, b2, c2, determ, determ1, determ2, x, y;
             Console.Write("Введіть коефіцієнт a1: ");
-            a1 = Convert.ToDouble(Console.ReadLine());
+            double a1 = Convert.ToDouble(Console.ReadLine());
+            
             Console.Write("Введіть коефіцієнт b1: ");
-            b1 = Convert.ToDouble(Console.ReadLine());
+            double b1 = Convert.ToDouble(Console.ReadLine());
+            
             Console.Write("Введіть праву частину c1: ");
-            c1 = Convert.ToDouble(Console.ReadLine());
+            double c1 = Convert.ToDouble(Console.ReadLine());
+            
             Console.Write("Введіть коефіцієнт a2: ");
-            a2 = Convert.ToDouble(Console.ReadLine());
+            double a2 = Convert.ToDouble(Console.ReadLine());
+            
             Console.Write("Введіть коефіцієнт b2: ");
-            b2 = Convert.ToDouble(Console.ReadLine());
+            double b2 = Convert.ToDouble(Console.ReadLine());
+            
             Console.Write("Введіть праву частину c2: ");
-            c2 = Convert.ToDouble(Console.ReadLine());
-            determ = a1 * b2 - a2 * b1;
-            determ1 = c1 * b2 - b1 * c2;
-            determ2 = a1 * c2 - a2 * c1;
-            x = determ1 / determ;
-            y = determ2 / determ;
+            double c2 = Convert.ToDouble(Console.ReadLine());
+            
+            double determ = a1 * b2 - a2 * b1;
+            double determ1 = c1 * b2 - b1 * c2;
+            double determ2 = a1 * c2 - a2 * c1;
+            
+            double x = determ1 / determ;
+            double y = determ2 / determ;
             Console.Write($"Відповідь: x={x}, y={y}");
         }
     }
