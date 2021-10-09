fc5a5ba4fb54cefd366dc1a4dcb53613efff0cad
demianchuk-oleksandr-IP13 <90649585+demianchuk-oleksandr-IP13@users.noreply.github.com>
2021-10-09 20:25:08 +0300

    Add files via upload

---
+++ lab3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter the value of a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            double eps = Math.Pow(10, -4);
+            double prevY;
+            double lastY = 1;
+
+            if (a < 0)
+            {
+                Console.WriteLine("Negative numbers are not suitable for this calculation.");
+            }
+            else
+            {
+                do
+                {
+                    prevY = lastY;
+                    lastY = (prevY + (a / prevY)) / 2;
+                                 }
+                while (Math.Abs(lastY - prevY) > eps);
+
+                Console.WriteLine(lastY);
+            }
+        }
+    }
+}
